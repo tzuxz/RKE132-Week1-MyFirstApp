afb56de8f957baf8efe5fc38a2caa9b254ec0d7f
tzuxz <juhanlokkota@gmail.com>
2023-01-30 22:00:18 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada nimi
+//rakendus tervitab kasutajat nime pidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello, " + userName + "!"); //output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
